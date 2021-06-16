d4bc4d6af363c519bdefb4dd22a272a405632271
PokeStar04 <romain.monfret@hetic.net>
2021-06-16 22:01:49 +0200

    Maj à jour swipper + new js + correction + responsive + feed

--- script/start-popup.js
+++ script/start-popup.js
@@ -1,19 +1,15 @@
-$(()=>{
-    $('.popup').each(function(){
-      $(this).toggleClass('active')
-      $('.popup-content').toggleClass('active')
-  })
-  $('.btn-close').click(function(){
-      $('.popup').removeClass('active')
-      $('.popup-content').removeClass('active')
-      $('.blur_active').removeClass('blur_active')
-      $(".croix").css('display', 'none');
-
-  })
-  $('#blur').each(function(){
-    $(this).toggleClass('blur_active')
-})
+$(() => {
+    $(".popup").each(function () {
+        $(this).toggleClass("active");
+        $(".popup-content").toggleClass("active");
+    });
+    $(".btn-close").click(function () {
+        $(".popup").removeClass("active");
+        $(".popup-content").removeClass("active");
+        $(".blur_active").removeClass("blur_active");
+        $(".croix").css("display", "none");
+    });
+    $("#blur").each(function () {
+        $(this).toggleClass("blur_active");
+    });
 });
-
-
-
